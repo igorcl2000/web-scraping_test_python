018277165def73e0b3654cabaa0d9e568325808f
igorcl2000 <igorcl2000@gmail.com>
2023-06-13 11:23:09 -0300

    001

--- scrapping.py
+++ scrapping.py
@@ -4,7 +4,8 @@ import re
 import pandas as pd
 import math
 
-url = 'https://www.kabum.com.br/cadeiras/cadeiras-gamer'
+#url = 'https://www.kabum.com.br/cadeiras/cadeiras-gamer'
+url = 'https://www.kabum.com.br/computadores/notebooks'
 
 headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"}
 
